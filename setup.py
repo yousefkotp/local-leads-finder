594746edc0318ccc390ecdd5a62052cc5a908749
Yousef Kotp <yousefkotp@outlook.com>
2025-10-13 17:17:13 -0400

    dev: update version to v1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="leads-finder",
-    version="1.0.0",
+    version="1.0.1",
     description="Local business leads finder using Decodo proxies",
     long_description=long_description,
     long_description_content_type="text/markdown",
